63861864aa9f3238a59e273de64fd4e45d1d6a27
Angel <aillescas@correo.ugr.es>
2019-03-27 15:59:03 +0100

    Fibonacci program

---
+++ Fibonacci.cs
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+
+class Program
+{
+    public static int Fibonacci(int n)
+    {
+        int a = 0;
+        int b = 1;
+        // In N steps compute Fibonacci sequence iteratively.
+        for (int i = 0; i < n; i++)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a;
+    }
+
+    static void Main()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            Console.WriteLine(Fibonacci(i));
+        }
+    }
+}
+
